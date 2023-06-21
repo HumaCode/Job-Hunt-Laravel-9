b05c9b48d0773b49273beb8016439b71b2e8c8f4
humaidi <humaidizakaria1998@gmail.com>
2023-06-21 09:33:43 +0700

    Login & Forget Password

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
+
+// auth
+Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+Route::get('/admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
