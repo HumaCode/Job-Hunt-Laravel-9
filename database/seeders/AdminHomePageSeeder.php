ce812c40912f6755865ebfb6263855652de16a6a
HumaCode <HumaCode>
2023-06-24 23:34:01 +0700

    Refactor AdminHomePageSeeder

--- database/seeders/AdminHomePageSeeder.php
+++ database/seeders/AdminHomePageSeeder.php
@@ -16,15 +16,18 @@ class AdminHomePageSeeder extends Seeder
     public function run()
     {
         PageHomeItem::create([
-            'heading'       => 'Find Your Desired Job',
-            'text'          => 'Search the best, perfect and suitable jobs that matches your skills in your expertise area.',
-            'job_title'     => 'Job Title',
-            'job_category'  => 'Job Category',
-            'job_location'  => 'Job Location',
-            'search'        => 'Search',
-            'background'    => '',
-            'created_at'    => date('Y-m-d H:i:s', time()),
-            'updated_at'    => date('Y-m-d H:i:s', time()),
+            'heading'                   => 'Find Your Desired Job',
+            'text'                      => 'Search the best, perfect and suitable jobs that matches your skills in your expertise area.',
+            'job_title'                 => 'Job Title',
+            'job_category'              => 'Job Category',
+            'job_location'              => 'Job Location',
+            'search'                    => 'Search',
+            'background'                => '',
+            'job_category_heading'      => 'Job Categories',
+            'job_category_subheading'   => 'Get the list of all the popular job categories here',
+            'job_category_status'       => 'Show',
+            'created_at'                => date('Y-m-d H:i:s', time()),
+            'updated_at'                => date('Y-m-d H:i:s', time()),
         ]);
     }
 }
