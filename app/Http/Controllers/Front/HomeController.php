c994102dd8a7cc39c9b140804e049daad86ed0fe
HumaCode <HumaCode>
2023-06-24 23:49:26 +0700

    Show Category - Frontend

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\JobCategory;
 use App\Models\PageHomeItem;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         $home_page_data = PageHomeItem::where('id', 1)->first();
+        $job_categories = JobCategory::take(9)->get();
 
         if ($home_page_data->background != null) {
             $gmbr = 'storage/' . substr($home_page_data->background, -52);
@@ -18,6 +20,6 @@ class HomeController extends Controller
             $gmbr = 'dist/uploads/heading5.jpg';
         }
 
-        return view('front.home', compact('home_page_data', 'gmbr'));
+        return view('front.home', compact('home_page_data', 'gmbr', 'job_categories'));
     }
 }
