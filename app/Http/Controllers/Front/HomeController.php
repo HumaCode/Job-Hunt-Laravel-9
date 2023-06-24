e0abbbd3c6d5c2cbd86a784b86194961cfae9565
HumaCode <HumaCode>
2023-06-24 18:38:30 +0700

    Make Store Admin Home Page Finally

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\PageHomeItem;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('front.home');
+        $home_page_data = PageHomeItem::where('id', 1)->first();
+        $gmbr = substr($home_page_data->background, -52);
+
+        return view('front.home', compact('home_page_data', 'gmbr'));
     }
 }
