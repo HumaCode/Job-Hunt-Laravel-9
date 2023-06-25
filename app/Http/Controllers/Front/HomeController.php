80a818facef952f6fd2307095b5210e1c9e54836
HumaCode <HumaCode>
2023-06-25 19:27:03 +0700

    Show Testimonial - frontend part 2

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -17,15 +17,26 @@ class HomeController extends Controller
 
         if ($home_page_data->background != null) {
             $gmbr = 'storage/' . substr($home_page_data->background, -52);
-            $gmbr2 = 'storage/' . substr($home_page_data->why_choose_background, -52);
         } else {
             $gmbr = 'dist-front/uploads/heading5.jpg';
+        }
+
+        if ($home_page_data->why_choose_background != null) {
+            $gmbr2 = 'storage/' . substr($home_page_data->why_choose_background, -52);
+        } else {
             $gmbr2 = 'dist-front/uploads/banner3.jpg';
+        }
+
+        if ($home_page_data->testimonial_background != null) {
+            $gmbr3 = 'storage/' . substr($home_page_data->testimonial_background, -52);
+        } else {
+            $gmbr3 = 'dist-front/uploads/banner11.jpg';
         }
 
         $data = [
             'gmbr'                  => $gmbr,
             'gmbr2'                 => $gmbr2,
+            'gmbr3'                 => $gmbr3,
             'home_page_data'        => $home_page_data,
             'job_categories'        => JobCategory::take(9)->get(),
             'job_categories_select' => JobCategory::get(),
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -360,41 +360,42 @@
         </div>
     @endif
 
-
-    <div class="testimonial" style="background-image: url('/storage/uploads/banner11.jpg')">
-        <div class="bg"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="main-header">Our Happy Clients</h2>
+    @if ($home_page_data->testimonial_status == 'Show')
+        <div class="testimonial" style="background-image: url({{ asset($gmbr3) }})">
+            <div class="bg"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="main-header">Our Happy Clients</h2>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="testimonial-carousel owl-carousel">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="testimonial-carousel owl-carousel">
 
-                        @foreach ($testimonials as $testimonial)
-                            <div class="item">
-                                <div class="photo">
-                                    <img src="{{ \Storage::url($testimonial->photo) }}" alt="" />
+                            @foreach ($testimonials as $testimonial)
+                                <div class="item">
+                                    <div class="photo">
+                                        <img src="{{ \Storage::url($testimonial->photo) }}" alt="" />
+                                    </div>
+                                    <div class="text">
+                                        <h4>{{ $testimonial->name }}</h4>
+                                        <p>{{ $testimonial->designation }}</p>
+                                    </div>
+                                    <div class="description">
+                                        <p>
+                                            {{ $testimonial->comment }}
+                                        </p>
+                                    </div>
                                 </div>
-                                <div class="text">
-                                    <h4>{{ $testimonial->name }}</h4>
-                                    <p>{{ $testimonial->designation }}</p>
-                                </div>
-                                <div class="description">
-                                    <p>
-                                        {{ $testimonial->comment }}
-                                    </p>
-                                </div>
-                            </div>
-                        @endforeach
+                            @endforeach
 
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
 
     <div class="blog">
         <div class="container">
