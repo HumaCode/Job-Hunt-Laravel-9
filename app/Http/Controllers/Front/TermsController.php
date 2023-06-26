d5e4db51e7d71997f7b94e7fa9323f5a229a6207
HumaCode <HumaCode>
2023-06-26 22:42:15 +0700

    Show Terms - frontend

--- app/Http/Controllers/Front/TermsController.php
+++ app/Http/Controllers/Front/TermsController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\PageTermItem;
 use Illuminate\Http\Request;
 
 class TermsController extends Controller
 {
     public function index()
     {
-        return view('front.terms');
+        $term_item    = PageTermItem::where('id', 1)->first();
+
+        return view('front.terms', compact('term_item'));
     }
 }
--- resources/views/front/terms.blade.php
+++ resources/views/front/terms.blade.php
@@ -1,64 +1,32 @@
 @extends('front.layouts.app')
 
+@section('seo_title')
+    {{ $term_item->title }}
+@endsection
+
+@section('seo_meta_description')
+    {{ $term_item->meta_description }}
+@endsection
 
 @section('main_content')
-<div class="page-top" style="background-image: url('storage/uploads/banner.jpg')">
-    <div class="bg"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h2>Terms and Conditions</h2>
+    <div class="page-top" style="background-image: url('storage/uploads/banner.jpg')">
+        <div class="bg"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2> {{ $term_item->heading }}</h2>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="page-content">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing
-                    elit. Hic iure delectus, aperiam eius sed suscipit
-                    corporis quas, nisi dicta harum excepturi quis est
-                    id deserunt a, ipsa autem in distinctio. Lorem ipsum
-                    dolor sit amet consectetur adipisicing elit. Hic
-                    iure delectus, aperiam eius sed suscipit corporis
-                    quas, nisi dicta harum excepturi quis est id
-                    deserunt a, ipsa autem in distinctio.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing
-                    elit. Hic iure delectus, aperiam eius sed suscipit
-                    corporis quas, nisi dicta harum excepturi quis est
-                    id deserunt a, ipsa autem in distinctio. Lorem ipsum
-                    dolor sit amet consectetur adipisicing elit. Hic
-                    iure delectus, aperiam eius sed suscipit corporis
-                    quas, nisi dicta harum excepturi quis est id
-                    deserunt a, ipsa autem in distinctio.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing
-                    elit. Hic iure delectus, aperiam eius sed suscipit
-                    corporis quas, nisi dicta harum excepturi quis est
-                    id deserunt a, ipsa autem in distinctio. Lorem ipsum
-                    dolor sit amet consectetur adipisicing elit. Hic
-                    iure delectus, aperiam eius sed suscipit corporis
-                    quas, nisi dicta harum excepturi quis est id
-                    deserunt a, ipsa autem in distinctio.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing
-                    elit. Hic iure delectus, aperiam eius sed suscipit
-                    corporis quas, nisi dicta harum excepturi quis est
-                    id deserunt a, ipsa autem in distinctio. Lorem ipsum
-                    dolor sit amet consectetur adipisicing elit. Hic
-                    iure delectus, aperiam eius sed suscipit corporis
-                    quas, nisi dicta harum excepturi quis est id
-                    deserunt a, ipsa autem in distinctio.
-                </p>
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    {!! $term_item->content !!}
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
